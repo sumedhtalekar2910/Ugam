f1a85f2a922d4992126f45eb66e66734604d015b
Sumedh Talekar <stalekar@merkleinc.com>
2022-12-09 15:10:09 +0530

    servlet

--- core/src/main/java/com/Ugams/core/services/impl/FetchApiUrlImpl.java
+++ core/src/main/java/com/Ugams/core/services/impl/FetchApiUrlImpl.java
@@ -2,7 +2,6 @@ package com.ugams.core.services.impl;
 
 import com.ugams.core.config.FetchApiConfig;
 import com.ugams.core.services.FetchApiUrl;
-import com.ugams.core.services.OSGIConfig;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.metatype.annotations.Designate;
@@ -16,8 +15,8 @@ public class FetchApiUrlImpl implements FetchApiUrl {
 
     @Activate
     public void activate(FetchApiConfig fetchApiConfig){
-        singleuserurl = fetchApiConfig.SingleUserUrl();
-        multiuserurl= fetchApiConfig.MultiUserUrl();
+        singleuserurl = fetchApiConfig.singleUserUrl();
+        multiuserurl= fetchApiConfig.multiUserUrl();
     }
 
     @Override
